75d09dd3077c5e5e2519ee41fd68ab7d54ed6f3b
mathilde_h <mathilde.humeau@yahoo.com>
2022-04-13 12:48:24 +0100

    added selection dropdown list to select number of users

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
 const randomFolks = document.querySelector(".random-peeps");
+const list = document.querySelector(".num-users");
+const selectUserNumber = document.querySelector("#users");
 
-console.log("test");
+const getData = async function(numUsers){
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
+    const data = await usersRequest.json();
+    const userResults = data.results;
+    console.log(userResults);
+    displayUsers(userResults)
+}   
+
+getData(1);
+
+const displayUsers = function(userResults){
+    randomFolks.innerHTML = "";
+    for (const user of userResults){
+    const country = user.location.country;
+    const name = user.name.first;
+    const imageUrl = user.picture.medium;
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `
+    <h3>${name}</h3>
+    <p>${country}</p>
+    <img src=${imageUrl} alt="User avatar" />`;
+    randomFolks.append(userDiv);
+    }
+
+};
+
+
+list.classList.remove("hide");
+
+selectUserNumber.addEventListener("change", function(e){
+    numUsers = e.target.value;
+    getData(numUsers);
+
+})
+
+
+
